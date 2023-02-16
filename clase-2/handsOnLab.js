c1423ebb53c8d52d108e9fb5a83b55ba0b3404fb
marcelocoria90 <coria.marcelo.90@gmail.com>
2023-02-16 09:32:53 -0300

    inicio handsOnLabs clase-2

---
+++ clase-2/handsOnLab.js
@@ -0,0 +1,20 @@
+/*
+
+productoManager.addProduct(new Product("Producto prueba", "Esto es un producto de prueba", 200, "Sin Imagen", "abc123", 25))
+
+*/
+
+
+/**
+ * Definir clase TicketManager, el cual tendrá un arreglo de eventos que iniciará vacío.
+ * La clase debe contar con una variable privada “precioBaseDeGanancia”, la cual añadirá un 
+costo adicional al precio de cada evento.
+ */
+
+class TickectManager {
+    #precioBaseDeGanancia = 0.15
+
+    constructor() {
+        this.events = []
+    }
+}
